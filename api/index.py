57880ecd8b858aae367820582ab683072b5aec70
sahil <sharma_sahil_2077@outlook.com>
2024-05-19 01:21:47 +0530

    step 2 done

--- api/index.py
+++ api/index.py
@@ -4,6 +4,10 @@ import os
 import io
 from PIL import Image
 from flask_cors import CORS
+import json 
+import numpy as np
+from collections import Counter
+import cv2
 
 app = Flask(__name__)
 CORS(app)
@@ -17,7 +21,6 @@ def home():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    print(f'request.files',request.files['image'])
     # print(f'request.form',request.form)
     if 'image' not in request.files:
         return jsonify({'error': 'No image uploaded'}), 400
@@ -33,17 +36,80 @@ def predict():
         image = Image.open(io.BytesIO(image_bytes))
 
         # Run inference
-        results = model(image, conf=conf_threshold)
+        results = model(image, conf=conf_threshold,show_boxes=False)
+        
         for result in results:
-            # Save result image
-            result.save(filename='./result.jpg')
-
-        # Send the segmented image back to the frontend
+            save_path = os.path.join(os.path.dirname(__file__), 'result.jpg')
+            result.save(filename=save_path)
+        
         return send_file('./result.jpg', mimetype='image/jpeg', as_attachment=True)
 
     except Exception as e:
         print(f"Error during prediction: {e}")
         return jsonify({'error': 'An error occurred'}), 500
+    
+@app.route('/predict/list', methods=['POST'])
+def predict_list():
+    # print(f'request.form',request.form)
+    if 'image' not in request.files:
+        return jsonify({'error': 'No image uploaded'}), 400
+
+    # Get confidence threshold value from request
+    conf_threshold = float(request.form.get('confidence', 0.1))
+
+    try:
+        # Read image data
+        image_bytes = request.files['image'].read()
+        
+        # Convert image bytes to PIL image
+        image = Image.open(io.BytesIO(image_bytes))
+
+        # Run inference
+        
+        X = model(image, conf=conf_threshold)
+        for y in X:
+            k_list=y.boxes.cls.tolist() 
+            print(f'k.........',k_list)
+
+        
+        return jsonify(k_list)
+    
+    except Exception as e:
+        print(f"Error during prediction: {e}")
+        return jsonify({'error': 'An error occurred'}), 500
+
+
+@app.route('/predict/path', methods=['POST'])
+def predict_path():
+    # print(f'request.form',request.form)
+    if 'image' not in request.files:
+        return jsonify({'error': 'No image uploaded'}), 400
+
+    # Get confidence threshold value from request
+    conf_threshold = float(request.form.get('confidence', 0.1))
+
+    try:
+        # Read image data
+        image_bytes = request.files['image'].read()
+
+        nparr = np.frombuffer(image_bytes, np.uint8)
+        image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+        # Run inference
+        X = model(image, conf=conf_threshold, classes = [0,1,3,4])
+                
+        for y in X:
+            for mask in y.masks.xy:
+                pts = mask.astype(int).reshape((-1, 1, 2))
+                cv2.fillPoly(image, [pts], color=(0,255,0),)  # Draw polygon
+                path=os.path.join(os.path.dirname(__file__), 'path.jpg')
+                cv2.imwrite(path, image)
+
+        return send_file('./path.jpg', mimetype='image/jpeg', as_attachment=True)
+
+    except Exception as e:
+        print(f"Error during prediction: {e}")
+        return jsonify({'error': 'An error occurred'}), 500
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- app/components/Map.jsx
+++ app/components/Map.jsx
@@ -6,7 +6,8 @@ import MapStyleSelector from './StyleSelector';
 import axios from 'axios';
 
 const Map = () => {
-  const [resultImage, setResultImage] = useState(null);  const [map, setMap] = useState(null);
+  // const [counts, setCounts]
+  const [resultImage, setResultImage] = useState(null); const [map, setMap] = useState(null);
   const [zoomLevel, setZoomLevel] = useState(2); // Initial zoom level
   const [imageData, setImageData] = useState(null);
   const [threshold, setThreshold] = useState(50);
@@ -19,15 +20,16 @@ const Map = () => {
   const mapContainerRef = useRef(null); // Ref for map container
   const [confidence, setConfidence] = useState(0.1);
   const [image, setPng] = useState(null);
+  const [preds, setPreds] = useState(null);
 
   const mapStyles = [
-    { label: 'Satellite', value: 'satellite-v9',  image:'/sat.png' },
-    { label: 'Streets', value: 'streets-v12',  image:'/street.png' },
-    { label: 'Dark', value: 'dark-v11',  image:'/dark.png' },
-    { label: 'Light', value: 'light-v11',  image:'/light.png' },
-    { label: 'Satellite Streets', value: 'satellite-streets-v12',  image:'/sat2.png' },
-    { label: 'Navigation Day', value: 'navigation-day-v1',  image:'/day.png' },
-    { label: 'Navigation Night', value: 'navigation-night-v1',  image:'/night.png' }
+    { label: 'Satellite', value: 'satellite-v9', image: '/sat.png' },
+    { label: 'Streets', value: 'streets-v12', image: '/street.png' },
+    { label: 'Dark', value: 'dark-v11', image: '/dark.png' },
+    { label: 'Light', value: 'light-v11', image: '/light.png' },
+    { label: 'Satellite Streets', value: 'satellite-streets-v12', image: '/sat2.png' },
+    { label: 'Navigation Day', value: 'navigation-day-v1', image: '/day.png' },
+    { label: 'Navigation Night', value: 'navigation-night-v1', image: '/night.png' }
   ];
 
 
@@ -38,10 +40,10 @@ const Map = () => {
       style: `mapbox://styles/mapbox/${selectedStyle}`,
       center: [80, 20],
       zoom: zoomLevel,
-      
+
       projection: 'globe',
     });
-  console.log(map.zoom)
+    console.log(map.zoom)
     map.on('style.load', () => {
       map.setFog({
         color: 'rgb(186, 210, 235)',
@@ -51,42 +53,42 @@ const Map = () => {
         'star-intensity': 0.6
       });
     });
-  
+
     setMap(map);
-  
+
     const rotateMap = () => {
       const bearing = map.getBearing() + 2;
       map.easeTo({ bearing: bearing });
     };
-  
+
     intervalRef.current = setInterval(rotateMap, 50);
-  
+
     const stopRotationOnClick = () => {
       clearInterval(intervalRef.current);
       map.off('click', stopRotationOnClick);
     };
-    
+
     map.on('zoom', () => {
       setZoomLevel(map.getZoom());
     });
 
 
     map.on('click', stopRotationOnClick);
-  
+
     return () => {
-      
-        // Cleanup: remove event listener on unmount
-        map.off('zoom', () => {
-          setZoomLevel(mapInstance.getZoom());
-        });
-     
+
+      // Cleanup: remove event listener on unmount
+      map.off('zoom', () => {
+        setZoomLevel(mapInstance.getZoom());
+      });
+
       clearInterval(intervalRef.current);
       map.off('click', stopRotationOnClick);
     };
   }, [selectedStyle]);
-  
 
- 
+
+
   const handleChange = (e) => {
     const { value } = e.target;
     setSearchQuery(value);
@@ -98,10 +100,10 @@ const Map = () => {
       console.log('true')
       try {
         const response = await fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(query)}.json?access_token=pk.eyJ1Ijoic2FoaWx4IiwiYSI6ImNsdW56eWg1czFqaTkybW4ycGZka2ZkYTUifQ.GTewW_1jNHqC4C_0cb0zNg`);
-      console.log('searched')
-        
+        console.log('searched')
+
         const data = await response.json();
-      console.log('data',data)
+        console.log('data', data)
 
         if (data.features.length > 0) {
           setSearchResults(data.features);
@@ -120,7 +122,7 @@ const Map = () => {
     const [longitude, latitude] = result.center;
     map.flyTo({ center: [longitude, latitude], zoom: 16 });
     clearInterval(intervalRef.current);
-    
+
     setSearchQuery('');
     setSearchResults([]);
   };
@@ -140,12 +142,12 @@ const Map = () => {
         throw new Error('Failed to fetch static map image');
       }
       const imageBlob = await response.blob();
-      
+
       // Convert image to PNG
       const fileReader = new FileReader();
       fileReader.onload = () => {
         const imageUrl = fileReader.result;
-  
+
         const canvas = document.createElement('canvas');
         const ctx = canvas.getContext('2d');
         const img = new Image();
@@ -154,7 +156,7 @@ const Map = () => {
           canvas.height = img.height;
           ctx.drawImage(img, 0, 0);
           const pngimage = canvas.toDataURL('image/png');
-          console.log('iiiiiiiiiiiiiiiiii',pngimage)
+          console.log('iiiiiiiiiiiiiiiiii', pngimage)
           // Save both original image and PNG in state variables
           setScreenshot(imageUrl);
           setPng(pngimage);
@@ -163,7 +165,7 @@ const Map = () => {
         img.src = imageUrl;
       };
       fileReader.readAsDataURL(imageBlob);
-    
+
     } catch (error) {
       console.error('Error capturing screenshot:', error);
     }
@@ -178,87 +180,118 @@ const Map = () => {
   //     console.error('Error capturing screenshot:', error);
   //   }
   // };
- 
-  
+
+
   const sendImageForPrediction = async () => {
     try {
-        // Convert the screenshot object to base64
-        const image = screenshot
+      // Convert the screenshot object to base64
+      const image = screenshot
 
-        await axios({
-            method: "POST",
-            url: "https://classify.roboflow.com/tree-segmentation-8bjvl/5",
-            params: {
-                api_key: "iai1ejzQyiSEer0AALCB"
-            },
-            data: image,
-            headers: {
-                "Content-Type": "application/x-www-form-urlencoded"
-            }
-        })
-        .then(function(response) {
+      await axios({
+        method: "POST",
+        url: "https://classify.roboflow.com/tree-segmentation-8bjvl/5",
+        params: {
+          api_key: "iai1ejzQyiSEer0AALCB"
+        },
+        data: image,
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded"
+        }
+      })
+        .then(function (response) {
           setImageData(response.data);
-            console.log(response.data);
+          console.log(response.data);
         })
-        .catch(function(error) {
-            console.log(error.message);
+        .catch(function (error) {
+          console.log(error.message);
         });
     } catch (error) {
-        console.error(error);
+      console.error(error);
     }
-};
+  };
 
-const handlePrediction = () => {
+  const handlePrediction = () => {
     setResultImage(null);
-  
+
     sendImageForPrediction();
-}
-  
-const handlePredict = async () => {
-  try {
-    
-    setImageData(null);
-    const byteString = atob(image.split(",")[1]);
-    const arrayBuffer = new ArrayBuffer(byteString.length);
-    const uint8Array = new Uint8Array(arrayBuffer);
-    for (let i = 0; i < byteString.length; i++) {
-      uint8Array[i] = byteString.charCodeAt(i);
-    }
-    const blob = new Blob([uint8Array], { type: 'image/jpeg' });
-    const file = new File([blob], "image.jpg", { type: 'image/jpeg' })
-    const formData = new FormData();
-    formData.append("image", file);
-    formData.append("confidence", confidence);
-    const response = await axios.post("http://localhost:5000/predict", formData, {
-      responseType: 'blob',
-      headers: {
-        "Content-Type": "multipart/form-data",
-      },
-    });
-    console.log(response.data)
-    setResultImage(URL.createObjectURL(response.data));
-   
-    
-  } catch (error) {
-    console.error("Error:", error);
   }
-};
+
+  const handlePredict = async () => {
+    try {
+
+      setImageData(null);
+      const byteString = atob(image.split(",")[1]);
+      const arrayBuffer = new ArrayBuffer(byteString.length);
+      const uint8Array = new Uint8Array(arrayBuffer);
+      for (let i = 0; i < byteString.length; i++) {
+        uint8Array[i] = byteString.charCodeAt(i);
+      }
+      const blob = new Blob([uint8Array], { type: 'image/jpeg' });
+      const file = new File([blob], "image.jpg", { type: 'image/jpeg' })
+      const formData = new FormData();
+      formData.append("image", file);
+      formData.append("confidence", confidence);
+      const response = await axios.post("http://localhost:5000/predict", formData, {
+        responseType: 'blob',
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+
+      console.log('respose', response)
+      setResultImage(URL.createObjectURL(response.data));
+      const response2 = await axios.post("http://localhost:5000/predict/list", formData, {
+
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+      console.log('respose2', response2)
+      const listData = response2.data;
+
+      // Map numbers to names
+      const numberToNameMap = {
+        0.0: 'Building',
+        1.0: 'Green',
+        2.0: 'Land',
+        3.0: 'Tree',
+        4.0: 'Water'
+      };
+
+      // Count occurrences of each number
+      const counts = {};
+      listData.forEach(number => {
+        const name = numberToNameMap[number];
+        counts[name] = counts[name] ? counts[name] + 1 : 1;
+
+
+      });
+      console.log('lsit datat', counts)
+
+      // Log counts
+      setPreds(counts)
+      console.log('pressssssssss', preds)
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
 
 
 
-const handleConfidenceChange = async (e) => {
-  setConfidence(e.target.value);
-  await handlePredict();
-};
+  const handleConfidenceChange = async (e) => {
+    setConfidence(e.target.value);
+    await handlePredict();
+
+  };
 
 
 
-  
-  
-  
-  
-  
-  
+
+
+
+
+
+
   const getMapParameters = () => {
     const center = map.getCenter();
     console.log('Center:', center);
@@ -280,6 +313,7 @@ const handleConfidenceChange = async (e) => {
     setResultImage(null);
     setImageData(null);
     setScreenshot(null); // Reset screenshot
+    setPreds(null);
   };
   const renderPredictions = () => {
     const displayedImageWidth = '40%'; // Adjust as needed
@@ -302,7 +336,7 @@ const handleConfidenceChange = async (e) => {
     };
   }, [showPreview]);
 
-    
+
 
 
   return (
@@ -346,7 +380,7 @@ const handleConfidenceChange = async (e) => {
         <div className='absolute right-0 top-0
         max-sm:h-10 max-sm:w-20
         ' >
-<MapStyleSelector mapStyles={mapStyles} selectedStyle={selectedStyle} handleStyleChange={handleStyleChange}/>
+          <MapStyleSelector mapStyles={mapStyles} selectedStyle={selectedStyle} handleStyleChange={handleStyleChange} />
         </div>
         {/* <div className="absolute top-4 right-4 text-black">
   <select value={selectedStyle} onChange={(e) => handleStyleChange(e.target.value)}>
@@ -362,107 +396,116 @@ const handleConfidenceChange = async (e) => {
         <div className="absolute justify-center top-[0%] left-[0%] snap-center w-full h-full flex  bg-gray-900 bg-opacity-75 z-20
         max-sm:h-[screen] max-sm:w-[screen] 
         ">
-         <div className="absolute h-[full-100px] w-[1400px] m-10 bg-white p-4 rounded shadow-lg
+          <div className="absolute h-[full-100px] w-[1400px] m-10 bg-white p-4 rounded shadow-lg
          max-sm:h-[700px] max-sm:w-[380px] max-sm:flex-col  max-sm:justify-center max-sm:backdrop-blur-sm max-sm:bg-transparent max-sm:outline 
          ">
-  <button className="absolute -top-6 -right-6 bg-white text-gray-500  hover:text-gray-800
+            <button className="absolute -top-6 -right-6 bg-white text-gray-500  hover:text-gray-800
   max-sm:-top-8 max-sm:right-1
   " onClick={handleClosePreview}>Close</button> {/* Close button */}
-  <div className='flex h-full w-full
+            <div className='flex h-full w-full
   max-sm:h-[400px] max-sm:w-[400px] max-sm:flex-col
   '>
-  <div className='flex-col h-[600px] w-[1000px] 
+              <div className='flex-col h-[600px] w-[1000px] 
   max-sm:flex-col max-sm:h-[700px] max-sm:w-[350px]
   '>
-    <div className='outline' >
-  <img
-  
-    src={screenshot}
-    alt="Screenshot"
-    className={`h-[500px] w-[380px] ${resultImage !== null ? 'hidden':'block'}
+                <div className='outline' >
+                  <img
+
+                    src={screenshot}
+                    alt="Screenshot"
+                    className={`h-[600px] w-[1000px] ${resultImage !== null ? 'hidden' : 'block'}
     max-sm:h-[400px] max-sm:w-[350px]
     `}
-  />
+                  />
 
-{resultImage && <img className=' absolute left-5 h-[600px] w-[1000px]
+                  {resultImage && <img className=' absolute left-5 h-[600px] w-[1000px]
 max-sm:h-[400px] max-sm:w-[350px] max-sm:outline 
 ' src={resultImage} alt="Result" />}
-            
-            <div className={`absolute -bottom-10 rounded-md h-10 w-[350px] justify-center items-center backdrop:blur-sm outline
-            max-sm:-bottom-14 ${resultImage !== null?'block':'hidden'}
+
+                  <div className={`absolute -bottom-10 rounded-md h-10 w-[350px] justify-center items-center backdrop:blur-sm outline
+            max-sm:-bottom-14 ${resultImage !== null ? 'block' : 'hidden'}
             `}>
-{resultImage && <div className=' flex m-2 '>
-             <input
-             className=' backdrop-blur-md
+                    {resultImage && <div className=' flex m-2 '>
+                      <input
+                        className=' backdrop-blur-md
              max-sm:
              '
-        type="range"
-        min="0.01"
-        max="1"
-        step="0.01"
-        value={confidence}
-        onChange={handleConfidenceChange}
-      />
-      
-      <span className='ml-2'>   {confidence}    Confidence Level</span> </div>}
-</div>
-      
-  <div className='absolute -top-6 left-4 '>
-      {imageData && (
-        
-        <div className='absolute -bottom-10 rounded-md  w-[350px]  bg-black outline outline-yellow-300 '>
-          <input
-        type="range"
-        min="0"
-        max="1"
-        step="0.01"
-        value={threshold}
-        onChange={handleChangeThreshold}
-        className="h-6 w-40"
-      />
-        {/* <img src={screenshot} alt="Your Image" style={{ maxWidth:'100%' }} /> */}
-        <div className='outline outline-slate-950 absolute  h-[600px] w-[1000px] object-contain '>
-        {imageData.predictions.map((prediction, index) => {
-          if (prediction.confidence >= threshold) {
-            return (
-              <div
-                className="absolute "
-                key={index}
-                style={{
-                  top: prediction.y/1.85 , // Adjusted position
-                  left: prediction.x/2.1 , // Adjusted position
-                  width: prediction.width/1.8 , // Adjusted width
-                  height: prediction.height/1.8, // Adjusted height
-                  border: '4px solid green',
-                  boxSizing: 'border-box',
-                }}
-              >{prediction.class}</div>
-            );
-          } else {
-            return null;
-          }
-        })}
-        </div>
-      </div>
-      
-      )}
-    </div>
-</div>
+                        type="range"
+                        min="0.01"
+                        max="1"
+                        step="0.01"
+                        value={confidence}
+                        onChange={handleConfidenceChange}
+                      />
 
-  </div>
-  <div className='flex-col w-[200px] justify-center items-center m-8
+                      <span className='ml-2'>   {confidence}    Confidence Level</span> </div>}
+                  </div>
+
+                  <div className='absolute -top-6 left-4 '>
+                    {imageData && (
+
+                      <div className='absolute -bottom-10 rounded-md  w-[350px]  bg-black outline outline-yellow-300 '>
+                        <input
+                          type="range"
+                          min="0"
+                          max="1"
+                          step="0.01"
+                          value={threshold}
+                          onChange={handleChangeThreshold}
+                          className="h-6 w-40"
+                        />
+                        {/* <img src={screenshot} alt="Your Image" style={{ maxWidth:'100%' }} /> */}
+                        <div className='outline outline-slate-950 absolute  h-[600px] w-[1000px] object-contain '>
+                          {imageData.predictions.map((prediction, index) => {
+                            if (prediction.confidence >= threshold) {
+                              return (
+                                <div
+                                  className="absolute "
+                                  key={index}
+                                  style={{
+                                    top: prediction.y / 1.85, // Adjusted position
+                                    left: prediction.x / 2.1, // Adjusted position
+                                    width: prediction.width / 1.8, // Adjusted width
+                                    height: prediction.height / 1.8, // Adjusted height
+                                    border: '4px solid green',
+                                    boxSizing: 'border-box',
+                                  }}
+                                >{prediction.class}</div>
+                              );
+                            } else {
+                              return null;
+                            }
+                          })}
+                        </div>
+                      </div>
+
+                    )}
+                  </div>
+                </div>
+
+              </div>
+              <div className='flex-col w-[200px] justify-center items-center m-8
   max-sm:flex
   '>
-  <button className=" h-10 w-20 justify-center text-gray-500  hover:text-gray-800 rounded-lg m-2  bg-slate-200" onClick={handlePrediction}>Predict</button> 
-  <div>
-            {/* Assuming you have a way to set the image state */}
-            
-            <button className=" h-10 w-40 justify-center text-gray-500  hover:text-gray-800 rounded-lg m-2 bg-slate-200" onClick={handlePredict}>Run Inference</button>
-        
-        </div>
-  </div>
-</div>
-</div>
+                <button className=" h-10 w-20 justify-center text-gray-500  hover:text-gray-800 rounded-lg m-2  bg-slate-200" onClick={handlePrediction}>Predict</button>
+                <div>
+                  {/* Assuming you have a way to set the image state */}
+
+                  <button className=" h-10 w-40 justify-center text-gray-500  hover:text-gray-800 rounded-lg m-2 bg-slate-200" onClick={handlePredict}>Run Inference </button>
+                  {preds && preds.hasOwnProperty('Tree') ? (
+                    <li className='h-10 w-50 text-black' key="Tree">
+                      Tree: {preds['Tree']}
+                    </li>
+                  ) : (
+                    <li className={`h-10 w-50 text-black ${preds === null?'hidden':'block'} `} key="Tree">
+                      No Tree Detected
+                    </li>
+                  )}
+
+                </div>
+              </div>
+            </div>
+          </div>
 
         </div>
       )}
